9030b2a1774dfdeef75a8e5952f6bd93676a78af
Darshilgor <gordarshilk2002@gmail.com>
2024-02-22 17:42:52 +0530

    22/02/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -15,7 +16,9 @@ Future main(List<String> args) async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseAppCheck.instance.activate();
   // FirebaseMessaging.onBackgroundMessage(_firebasemessagingBackgroundHandler);
-  runApp(MyApp());
+  runApp(DevicePreview(
+    builder: (BuildContext context) => MyApp(),
+  ));
 }
 
 @pragma('vm:entry-point')
